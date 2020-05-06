98d158396c89e63e62e8c1db9f12c4481f424605
Sergio López <sergiolf96@gmail.com>
2020-05-06 14:48:57 +0200

    Checkout is now included in NavLink

---
+++ frontend/view/checkout/CheckoutListView.swift
@@ -0,0 +1,21 @@
+//
+//  CheckoutListView.swift
+//  frontend
+//
+//  Created by Sergio López on 06/05/2020.
+//  Copyright © 2020 SHPL. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutListView()
+    }
+}
